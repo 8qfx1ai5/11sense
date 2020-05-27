f08152cec307e29ebf643a34549c15b6a60d72d1
Felix Ohnesorge <felix.ohnesorge@8qfx1ai5.de>
2020-05-27 19:24:54 +0200

    test back button

--- website/js/page/page_event.js
+++ website/js/page/page_event.js
@@ -25,8 +25,7 @@
         window.addEventListener('click', function(e) {
             if (firstClick) {
                 firstClick = false;
-                clickNavPage();
-                clickMainPage();
+                window.location.assign("#welcome");
             }
         });
     }
