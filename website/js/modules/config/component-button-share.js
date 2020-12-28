839a30e50b3982cdb7b0f9c0ab19b5634aa06e89
Felix Ohnesorge <felix.ohnesorge@8qfx1ai5.de>
2020-12-28 16:45:07 +0100

    replace share-button

---
+++ website/js/modules/config/component-button-share.js
@@ -0,0 +1,93 @@
+import * as appSystem from '../main/system.js'
+
+customElements.define('button-share', class extends HTMLElement {
+
+    constructor() {
+        super()
+
+        this.attachShadow({ mode: 'open' })
+
+        let title = this.getAttribute('title')
+        let label = this.getAttribute('label')
+        let sublabel = this.getAttribute('sublabel')
+        let isDisabled = this.hasAttribute('disabled')
+        let shareHeadline = this.hasAttribute('shareHeadline')
+        let shareText = this.hasAttribute('shareText')
+
+        this.shadowRoot.innerHTML = `
+            <style>
+                :host {
+                    
+                }
+
+                button {
+                    background-color: var(--theme-color-7);
+                    color: var(--theme-color-1);
+                    font-family: inherit;
+                    line-height: 0.9em;
+                    border: none;
+                    text-align: left;
+                    text-decoration: none;
+                    display: inline-block;
+                    font-size: 1.4em;
+                    cursor: pointer;
+                    width: 100%;
+                    margin-top: 5px;
+                    vertical-align: top;
+                    padding: 0.4em 1em 0.4em 1em;
+                    position: relative;
+                }
+
+                button:focus {
+                    outline: none;
+                }
+
+                .disabled {
+                    color: black;
+                    text-decoration: line-through !important;
+                    -webkit-tap-highlight-color: transparent;
+                }
+
+                #sublabel {
+                    font-size: 0.6em !important;
+                }
+
+                #label {
+                    display: inline-block;
+                }
+
+            </style>
+            <button title="${title}">
+                <span id="label">${label}</span>
+                <br />
+                <span id="sublabel">(${sublabel})</span>
+            </button>
+        `
+
+        let inputButton = this.shadowRoot.querySelector('button')
+        let labelSpan = this.shadowRoot.querySelector('#label')
+        let sublabelSpan = this.shadowRoot.querySelector('#sublabel')
+
+        if (isDisabled || !("share" in navigator)) {
+            // TODO: provide an alternative for sharing without navigator
+            inputButton.setAttribute("disabled", "disabled")
+            inputButton.classList.add("disabled")
+            labelSpan.classList.add("disabled")
+            sublabelSpan.classList.add("disabled")
+        }
+
+        inputButton.addEventListener('click', function(e) {
+            navigator
+                .share({
+                    title: shareHeadline,
+                    text: shareText,
+                    url: window.location.origin
+                })
+                .then(() => appSystem.log("Successful shared"), 2)
+                .catch(err => {
+                    appSystem.log(err, 2, "console")
+                    appSystem.log(err.error, 2, "app")
+                })
+        })
+    }
+})
