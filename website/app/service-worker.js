f348777a8e3cc60b1ee5c909d7fb52ba8374b200
Felix Ohnesorge <felix.ohnesorge@8qfx1ai5.de>
2021-01-03 23:50:07 +0100

    refactor service worker for version caching (broken)

--- website/app/service-worker.js
+++ website/app/service-worker.js
@@ -1,13 +1,34 @@
 const staticCacheName = 'pages-cache-v1'
-const cacheTimeoutInMs = 5000
+const cacheTimeoutInMs = 10000
+const headerVersionKey = 'version'
+const dbBuildVersionKey = 'version'
+const dbVersionUpdateTimeKey = 'versionTime'
+const dbName = 'meta'
+const dbStoreName = '11sense'
 
 let version = ""
-
 let versionTime = 0
+let store
+let idbRequest
+let db
 
-self.addEventListener('install', event => {})
+// self.addEventListener('push', event => {});
+
+// self.addEventListener('install', event => {})
+
+self.addEventListener('activate', function(event) {
+    if (!self.indexedDB) {
+        console.log('IndexedDB not supported');
+        return
+    }
+
+    event.waitUntil(
+        createDB()
+    )
+})
 
 self.addEventListener('fetch', function(event) {
+
     let currentRequest = event.request
     if (event.request.url.endsWith('.js')) {
         // handle js module loads exceptional, because of the different request behavior,
@@ -20,23 +41,50 @@ self.addEventListener('fetch', function(event) {
 
     event.respondWith(caches.match(currentRequest)
         .then(cachedResponse => {
-            if (cachedResponse && performance.now() - versionTime < cacheTimeoutInMs && cachedResponse.headers.version == version) {
-                console.log('Found in cache ', currentRequest.url, ' (right version and in time)');
-                return cachedResponse;
+            if (cachedResponse) {
+                if (self.indexedDB) {
+                    idbRequest = self.indexedDB.open(dbName, 1);
+                    idbRequest.onsuccess = function(dbEvent) {
+                        let db = dbEvent.target.result
+                        let tx = db.transaction([dbStoreName], 'readonly');
+                        let store = tx.objectStore(dbStoreName);
+                        // TODO: here is a bug, wait until data returned
+                        version = store.get(dbBuildVersionKey);
+                        versionTime = store.get(dbVersionUpdateTimeKey);
+
+                        console.log('now:', performance.now(), ' versionTime:', versionTime, ' diff:', performance.now() - versionTime)
+                        if (cachedResponse && performance.now() - versionTime < cacheTimeoutInMs && cachedResponse.headers.version == version) {
+                            console.log('version:', version, ' cachedVersion:', cachedResponse.headers.version)
+                            console.log('Found in cache ', currentRequest.url, ' (right version and in time)');
+                            return cachedResponse;
+                        }
+
+                        // cache outdated
+                        return sendRequest(event)
+                    }
+                    idbRequest.onerror = function(error) {
+                        console.log('indexedDB access failed:', error)
+                        return sendRequest(event)
+                    }
+                } else {
+                    // IndexedDB is not supported
+                    return sendRequest(event)
+                }
             }
             // wrong version or outdated cache
             return sendRequest(currentRequest)
         }).catch(error => {
             // not in cache
             return sendRequest(currentRequest)
-        }))
+        })
+    )
 })
 
 function sendRequest(request) {
     return fetch(request)
         .then(response => {
-            versionTime = performance.now()
-            version = response.headers.version
+            store.put({ key: dbBuildVersionKey, value: response.headers.version })
+            store.put({ key: dbVersionUpdateTimeKey, value: performance.now() })
 
             // Add fetched files to the cache
             return caches.open(staticCacheName).then(cache => {
@@ -45,9 +93,8 @@ function sendRequest(request) {
                 return response
             })
         }).catch(error => {
-            console.log("Offline: ", request.url, ' try to use cache')
 
-            return caches.match(request)
+            return caches.match(request, { ignoreSearch: true })
                 .then(cachedResponse => {
                     if (cachedResponse) {
                         console.log('Found in cache (offline)', currentRequest.url);
@@ -61,12 +108,20 @@ function sendRequest(request) {
         })
 }
 
-// self.addEventListener('push', event => {
-//     event.waitUntil(
-//         self.registration.showNotification('Neuer Stadtführer verfügbar', {
-//             body: 'Entdecken Sie die Schönheiten der Baustellenstadt Karlsruhe.',
-//             icon: 'assets/launcher-icon-3x.png',
-//             tag: 'notification'
-//         })
-//     );
-// });
+function createDB() {
+    idbRequest = self.indexedDB.open(dbName, 1);
+    idbRequest.onsuccess = function(event) {}
+    idbRequest.onupgradeneeded = function(event) {
+        let db = event.target.result
+        let store = db.createObjectStore(dbStoreName, {
+            keyPath: 'key'
+        });
+        store.createIndex("value", "value", { unique: false });
+        // initiate values
+        store.put({ key: dbBuildVersionKey, value: "" });
+        store.put({ key: dbVersionUpdateTimeKey, value: 0 });
+    };
+    idbRequest.onerror = function(error) {
+        console.log("DB creation failed: ", error)
+    }
+}
